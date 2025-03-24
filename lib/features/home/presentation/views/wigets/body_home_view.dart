833b7c252cc3d33154438572b99a835a6726913f
youssef hamada <yosefhagag84171@gmail.com>
2025-03-24 16:48:44 +0200

    display data customslider

--- lib/features/home/presentation/views/wigets/body_home_view.dart
+++ lib/features/home/presentation/views/wigets/body_home_view.dart
@@ -3,39 +3,49 @@ import 'package:books_store/core/presentation/widgets/slider_card.dart';
 import 'package:books_store/core/resources/app_strings.dart';
 import 'package:books_store/core/resources/styles.dart';
 import 'package:books_store/core/router/app_routes.dart';
+import 'package:books_store/features/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart';
 import 'package:books_store/features/home/presentation/views/wigets/newest_list_view_bloc_builder.dart';
 
 import 'package:books_store/features/home/presentation/views/wigets/porpodals_list_view_bloc_builder.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class BodyHomeView extends StatelessWidget {
-  BodyHomeView({super.key});
-  final List<String> imagelist = [
-    'https://s26162.pcdn.co/wp-content/uploads/2017/04/42179u-Edit_500_2048x2048.jpg',
-    'https://dm7xpch4tw6cs.cloudfront.net/images/template/5099/29/books-poster__front.jpg',
-    'https://dbdzm869oupei.cloudfront.net/img/posters/preview/88050.png',
-    'https://render.fineartamerica.com/images/rendered/default/flat/puzzle/images/artworkimages/medium/3/books-into-the-library-poster-art-print-decor-home-sheryl-neal.jpg?&targetx=0&targety=-61&imagewidth=750&imageheight=1122&modelwidth=750&modelheight=1000&backgroundcolor=13120B&orientation=1&producttype=puzzle-18-24&brightness=580&v=6',
-  ];
+ const BodyHomeView({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
-        SliverToBoxAdapter(
-          child: CustomSlider(
-            itemBuilder: (BuildContext context, int itemIndex, index) {
-              return SliderCard(
-                itemIndex: itemIndex,
-                imageUrl: imagelist[itemIndex],
-                onTap: () {
-                  context.pushNamed(AppRoutes.homedetailsRoute);
+        BlocBuilder<FeatureBooksCubit, FeatureBooksState>(
+          builder: (context, state) {
+             
+            if (state is FeatureBooksSuccess) {
+               final limitedBooks = state.books.take(4).toList(); // أخذ أول 4 كتب فقط
+               return SliverToBoxAdapter(
+              child: CustomSlider(
+                itemBuilder: (BuildContext context, int itemIndex, index) {
+                  return SliderCard(
+                    itemIndex: itemIndex,
+                    imageUrl: limitedBooks[itemIndex].image??'',
+                    onTap: () {
+                      context.pushNamed(AppRoutes.homedetailsRoute);
+                    },
+                    title:limitedBooks[itemIndex].title?? 'No Title',
+                    date: limitedBooks[itemIndex].date??'No Date',
+                  );
                 },
-                title: 'titl book',
-                date: '20 .2 .2025',
-              );
-            },
-          ),
+              ),
+            );
+            } else if(state is FeatureBooksFailure){
+                return  SliverToBoxAdapter(child: Center(child: Text(state.errormessage)));
+            } else{
+ return  SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+            }
+           
+          },
         ),
         SliverToBoxAdapter(
           child: Padding(
@@ -55,4 +65,3 @@ class BodyHomeView extends StatelessWidget {
     );
   }
 }
-
