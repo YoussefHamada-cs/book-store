2347446c30fa380ce9f5d467b5e82d22167037bc
youssef hamada <yosefhagag84171@gmail.com>
2025-03-28 14:36:36 +0200

    do entites search

---
+++ lib/features/search/domain/entites/book.dart
@@ -0,0 +1,30 @@
+import 'package:hive/hive.dart';
+part 'book.g.dart';
+
+@HiveType(typeId: 0)
+class Book {
+  @HiveField(1)
+  final String? image;
+  @HiveField(2)
+  final String? title;
+  @HiveField(3)
+  final List<String> authors;
+  @HiveField(4)
+  final String? date;
+  @HiveField(5)
+  final int? pageCount;
+  @HiveField(6)
+  final String goTorReadBook;
+  @HiveField(7)
+   final String goTorGoogleStore;
+  @HiveField(8)
+   final List<String>? categories;
+  Book(  {   required this.categories,
+    required  this.goTorReadBook, required  this.goTorGoogleStore,
+    required this.pageCount,
+    required this.title,
+    required this.authors,
+    required this.date,
+    required this.image,
+  });
+}
