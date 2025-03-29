0d75c36625101867a384a6dd766008420ad59ff0
youssef hamada <yosefhagag84171@gmail.com>
2025-03-30 01:03:11 +0200

    stay favoriteitem at restart app

--- lib/features/favorite/presentation/manager/favorite_cubit.dart
+++ lib/features/favorite/presentation/manager/favorite_cubit.dart
@@ -1,19 +1,49 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:books_store/features/home/domain/entites/book.dart';
-
+import 'package:books_store/core/resources/app_strings.dart';
 class FavoriteCubit extends Cubit<List<Book>> {
-  FavoriteCubit() : super([]);
+  final Box<Book> _favoritesBox;
+
+  FavoriteCubit() :
+      _favoritesBox = Hive.box<Book>(AppStrings.kfavoritesBox),
+      super([]) {
+    _loadFavorites();
+  }
+
+  // تحميل البيانات من Hive
+  void _loadFavorites() {
+    final favorites = _favoritesBox.values.toList();
+    emit(favorites);
+  }
 
   // إضافة كتاب إلى المفضلة
   void addToFavorites(Book book) {
     if (!state.contains(book)) {
-      emit([...state, book]);
+      _favoritesBox.add(book); // إضافة الكتاب إلى Hive
+      final updatedFavorites = [...state, book];
+      emit(updatedFavorites);
     }
   }
 
   // إزالة كتاب من المفضلة
   void removeFromFavorites(Book book) {
-    emit(state.where((b) => b != book).toList());
+    final key = _getKeyForBook(book);
+    if (key != null) {
+      _favoritesBox.delete(key); // حذف الكتاب من Hive
+      final updatedFavorites = state.where((b) => b != book).toList();
+      emit(updatedFavorites);
+    }
+  }
+
+  // الحصول على المفتاح الخاص بالكتاب في Hive
+  int? _getKeyForBook(Book book) {
+    for (var i = 0; i < _favoritesBox.length; i++) {
+      if (_favoritesBox.getAt(i) == book) {
+        return i;
+      }
+    }
+    return null;
   }
 
   // التحقق مما إذا كان الكتاب موجودًا في المفضلة
